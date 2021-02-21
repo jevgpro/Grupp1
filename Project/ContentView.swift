cbf29a528709522b2c5dcdf21cc7c279d7a923fd
Mark-Kirill Gubal <55079821+markgub@users.noreply.github.com>
2021-02-21 12:03:06 +0200

    Initial project with "Hello, dear user!" message
    Right now app features only "Hello, dear user!" with blue background on a white background

---
+++ Project/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Project
+//
+//  Created by User on 21.02.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, dear user!")
+            .padding(.all)
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Project/ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectApp.swift
+//  Project
+//
+//  Created by User on 21.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
