984469b6de6293d146fb30d17f97d3a35d2053d0
Mark-Kirill Gubal <55079821+markgub@users.noreply.github.com>
2021-02-22 12:13:15 +0200

    Added new text and changed existing one
    Sai lisatud uus tekst "There is nothing here right now" ning oli muudetud "Hello, dear user!" teksti kaal.

--- Project/ContentView.swift
+++ Project/ContentView.swift
@@ -10,8 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         Text("Hello, dear user!")
+            .fontWeight(.bold)
             .padding(.all)
             .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
+        Text("There is nothing to see right now")
+            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.red/*@END_MENU_TOKEN@*/)
+            
+            
     }
 }
 
