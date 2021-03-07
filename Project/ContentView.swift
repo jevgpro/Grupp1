0ccad32e6f013f314ec944f10033801c1d97e0d1
Mark-Kirill Gubal <55079821+markgub@users.noreply.github.com>
2021-03-07 19:19:32 +0200

    Added 3 basic pages to the app

--- Project/ContentView.swift
+++ Project/ContentView.swift
@@ -9,19 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, dear user!")
-            .fontWeight(.bold)
-            .padding(.all)
-            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.blue/*@END_MENU_TOKEN@*/)
-        Text("There is nothing to see right now")
-            .background(/*@START_MENU_TOKEN@*//*@PLACEHOLDER=View@*/Color.red/*@END_MENU_TOKEN@*/)
-            
-            
+        NavigationView{
+            page1().navigationBarTitle("Page 1", displayMode: .inline)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct page1: View {
+    var body: some View {
+        VStack{
+            Text("This is page 1")
+                .fontWeight(.heavy)
+            NavigationLink(destination: page2()){
+                Text("Go to page 2")
+            }
+        }
+    }
+}
+
+struct page2: View {
+    var body: some View {
+        VStack{
+            Text("This is page 2")
+                .fontWeight(.heavy)
+            NavigationLink(destination: page3()){
+                Text("Go to page 3")
+            }
+        }
+    }
+}
+
+struct page3: View {
+    var body: some View {
+        VStack{
+            Text("This is page 3")
+                .fontWeight(.heavy)
+            NavigationLink(destination: page2()){
+                Text("Go back to page 2")
+            }
+        }
     }
 }
